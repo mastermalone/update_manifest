1814209920eb0b7992e4af537c325fae42a2735a
michael.malone@aofl.com <michael.malone@aofl.com>
2017-10-28 11:17:55 -0700

    Tweaked the update_manifest script to wait for all processes to finish.

--- update_manifest.sh
+++ update_manifest.sh
@@ -6,8 +6,15 @@ relativePath="$(cd "$(dirname "$1")"; pwd)/"
 
 function update_manifest() {
   #echo $updateTextFile
+  
+  if [[ -f m_update_results.txt ]];
+  then
+  	rm m_update_results.txt
+  fi
+  
   touch m_update_results.txt;
   m_u_results=m_update_results.txt;
+	updatedFileCount=0;
   
   while IFS='' read -r line
   do
@@ -16,13 +23,15 @@ function update_manifest() {
     dir_name="${fspec%/*}"; # get directory/path name
     manifestFile=index;
     
-    cd $dir_name; #TODO Remove the back dir
+    cd $dir_name; 
     
     echo $filename >> $manifestFile;
     echo "Successfully added $filename to $dir_name/init" >> $relativePath$m_u_results;
     echo "Adding $filename";
-    
+    updatedFileCount=`expr $updatedFileCount + 1`
   done < "$updateTextFile"
+  wait 
+  echo "The process has finished.  There were/was $updatedFileCount file/s have been updated.";
 }
 
 update_manifest
